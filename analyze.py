ffc6476223aaf9bf0226d763aa729945688c4cf3
BennyTW <b04502051@g.ntu.edu.tw>
2020-03-18 12:02:38 +0800

    unaware

---
+++ analyze.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+A = pd.read(argv[2])
+B = pd.read(argv[3])
+print(A)
+        # with open(self.fp_list[idx][0], 'rb') as f:
+
+        #     # print(self.first_n_byte)
+        #     byte = f.read()
+        #     hexadecimal = binascii.hexlify(byte)
+        #     hexadecimal = [hexadecimal[i:i+2] for i in range(0, len(hexadecimal), 2)]
